406e1301a6ac375c98c085dd2a1e97e6701fa549
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-04 16:02:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   string = ""
   array.each_with_index do |element, index|
     if index == (array.length - 1)
-      string += ", and " + element
+      string += " and " + element
     else
       string += element + ", "
     end
