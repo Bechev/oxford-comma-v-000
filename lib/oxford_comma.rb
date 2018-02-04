6f31a70f2399c5e164ddd484572529c303f55b10
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-04 15:54:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   count = 0
   string = ""
   array.each_with_index do |element, index|
-    if count == array.size - 1
+    if count == (array.size - 1)
       string += ", and " + element
     else
       string += element + ", "
